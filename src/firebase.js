9a3877b2a3cd392fd4207321fa1c72ed7c8206a2
MajorPickaxe <taylan.ozdemir505@gmail.com>
2022-10-01 23:45:40 +1000

    pushing base app

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAUsgwk-3ACyYAoFNL2YemD0lzMGAg94PI",
+    authDomain: "group-z.firebaseapp.com",
+    projectId: "group-z",
+    storageBucket: "group-z.appspot.com",
+    messagingSenderId: "387745457753",
+    appId: "1:387745457753:web:4e295d1f200e6e2e417348",
+    measurementId: "G-8MTJYL5NTT"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
