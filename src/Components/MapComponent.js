6df0640734ae85c945e604871c85f71ad9fce569
Joshua Lewis <jrlewis.b@gmail.com>
2022-10-05 21:17:20 +1100

    Progress commit idk why it didnt commit the component

---
+++ src/Components/MapComponent.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import dotenv from "dotenv";
+import { useState } from "react";
+
+export default function MapComponent() {
+  const { isMapLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+  });
+
+  const [state, setState] = useState({ position: undefined });
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (successPosition) => {
+          setState({
+            ...state,
+            position: {
+              lat: successPosition.coords.latitude,
+              lng: successPosition.coords.longitude,
+            },
+          });
+        },
+        (errorPosition) => {
+          console.log(errorPosition);
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+  if (!isMapLoaded || state.position == undefined) {
+    return <div>Loading...</div>;
+  }
+  if (isMapLoaded && state.position != undefined) {
+  }
+
+  return <div>MapComponent</div>;
+}
