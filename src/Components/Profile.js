e45e44fba1f7f0b7724c768333505df3b855c14a
ishaq-ctrl <ishaq.siddiqui@students.mq.edu.au>
2022-11-06 20:32:13 +1100

    added a profile image incase none

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "../App.css";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading, user_metadata} = useAuth0();
+  const { user, isAuthenticated, isLoading} = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -12,7 +12,9 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={"cannot display image, login with google to see image"} />
+        <img src={user.picture}   onError={(e)=>{e.target.onerror = null; e.target.classList.add('incase_image'); e.target.src="https://cdn-icons-png.flaticon.com/512/1256/1256643.png?w=826&t=st=1667726667~exp=1667727267~hmac=7a8017865df9f2b46428b20b9f0c365b81dae4a306e5382603680ca41c8fc864"}}
+
+        alt={"cannot display image, login with google to see image"} />
         <p className="locale"> Locale - {user.locale} </p> 
         <h2>Name: {user.name} </h2>
         <p>Email: {user.email}</p>
